a25f03ca0b696f928b5e9a97482ae2e46cb2769f
TraquPjatk <s19787@pjwstk.edu.pl>
2023-05-22 14:13:00 +0200

    Thread breaking the script

--- macro_F.py
+++ macro_F.py
@@ -1,6 +1,7 @@
 import time
 import keyboard
 import datetime
+import threading
 
 #####################################################################################
 #####################################################################################
@@ -18,10 +19,16 @@ DELAY_BETWEEN_COMMANDS = 0.2
 def get_time():
     return datetime.datetime.now().time().strftime("%H:%M:%S")
 
+def format_time(execution_time):
+    execution_time_formatted = "{:.2f}".format(execution_time)
+    return execution_time_formatted
+
 #####################################################################################
 
 
 def press_and_hold_key(key, hold_time):
+    if stop_flag.is_set():
+            return
     keyboard.press(key)
     time.sleep(hold_time)
     keyboard.release(key)
@@ -30,41 +37,55 @@ def press_and_hold_key(key, hold_time):
 #####################################################################################
 
 def from_thousands_to_ones():
+    if stop_flag.is_set():
+            return
     print(get_time() + '  From thousands to ones')
     time.sleep(TIME_NEEDED_TO_EXIT_ANIMATION)
     for _ in range(3):
         press_and_hold_key('f', 0.1)
 
 def from_hundreeds_to_ones():
+    if stop_flag.is_set():
+            return
     print(get_time() + '  From hundreeds to ones')
     time.sleep(TIME_NEEDED_TO_EXIT_ANIMATION)
     for _ in range(2):
         press_and_hold_key('f', 0.1)    
 
 def from_hundreeds_to_tens():
+    if stop_flag.is_set():
+            return
     print(get_time() + '  From hundreeds to tens')
     time.sleep(TIME_NEEDED_TO_EXIT_ANIMATION)
     press_and_hold_key('f', 0.1)    
 
 def from_tens_to_ones():
+        if stop_flag.is_set():
+            return
         print(get_time() + '  From tens to ones')
         # konieczne żeby poczekać na wyjście z poprzedniej animacji
         time.sleep(TIME_NEEDED_TO_EXIT_ANIMATION)
         press_and_hold_key('f', 0.1)      
 
 def from_ones_to_tens():
+    if stop_flag.is_set():
+            return
     print(get_time() + '  From ones to tens')
     time.sleep(TIME_NEEDED_TO_EXIT_ANIMATION)
     for _ in range(3):
         press_and_hold_key('f', 0.1)
 
 def from_ones_to_hundreeds():
+    if stop_flag.is_set():
+            return
     print(get_time() + '  From ones to hundreeds')
     time.sleep(TIME_NEEDED_TO_EXIT_ANIMATION)
     for _ in range(2):
         press_and_hold_key('f', 0.1)
 
 def from_ones_to_thousands():
+    if stop_flag.is_set():
+            return
     print(get_time() + '  From ones to thousands')
     time.sleep(TIME_NEEDED_TO_EXIT_ANIMATION)
     press_and_hold_key('f', 0.1)
@@ -73,6 +94,8 @@ def from_ones_to_thousands():
 #####################################################################################
 
 def increment():
+    if stop_flag.is_set():
+            return
     print(get_time() + '  Increment')
     press_and_hold_key('f', server_hold_time_1)    
 
@@ -83,6 +106,8 @@ def setup():
 #####################################################################################
 
 def check_0001s_options(key, hold_time):
+    if stop_flag.is_set():
+            return
     print(get_time() + '  Checking ones')
     press_and_hold_key('f', server_hold_time_10)
    # time.sleep(1.0)
@@ -100,35 +125,42 @@ def check_0100s_options():
       increment()
       from_hundreeds_to_ones()
       check_0010s_options()
- #  from_ones_to_hundreeds()
- #  increment()
- #  from_hundreeds_to_tens()
- #  increment()
- #  from_tens_to_ones()
 
 def check_1000s_options():
+    if stop_flag.is_set():
+        return
     for _ in range(10):
+        if stop_flag.is_set():
+            return
         from_ones_to_thousands()
         increment()
         from_thousands_to_ones()
         check_0100s_options()
-       # for _ in range(10):
-         #   check_0010s_options()
-         #   check_0001s_options('f', server_hold_time_10)
-         #   check_0100s_options()
 
 #####################################################################################
 #####################################################################################
+
+# Tworzymy flagę, która będzie służyć do przerwania metody start_cracking_the_code()
+stop_flag = threading.Event()
+
+def stop_execution():
+    stop_flag.set()
+
+# Funkcja obsługująca wciśnięcie klawisza
+def key_press_handler(event):
+    if event.name == 'a' or event.name == 'w' or event.name == 's' or event.name == 'd' or event.name == 'esc' or event.name == 'space' or event.name == 'shift' or event.name == 'alt' or event.name == 'tab':
+        stop_execution()
+
+# Rejestrujemy funkcję obsługującą wciśnięcie klawisza przerywającego proces
+keyboard.on_press(key_press_handler)
 
 def start_cracking_the_code():
 
    setup() #starting pos. from ones (player runs the script at thousands)
-
- #  check_0010s_options()
- #  check_0100s_options()
    check_1000s_options()
-
-
+   if stop_flag.is_set():
+        print("\nScript has been cancelled\n")
+        return
 
 #####################################################################################
 #####################################################################################
@@ -145,15 +177,20 @@ for x in range(0, 3):
     countDownTimer=countDownTimer-1
     time.sleep(1)
 time.sleep(0.5)
-print('\n' + get_time() + '  \nCRACKING IS BEING PERFORMED!\n')
+print('\n' + get_time() + ' EXECUTION START TIME\n\n\nCRACKING LOGS:\n')
 
 start_time = time.time()
 start_cracking_the_code()
 end_time = time.time()
 
 
-print(get_time() + "Lock opening finished.")
+print(get_time() + "  Lock opening finished.\n")
 execution_time = end_time - start_time
-print("Script execution time:", execution_time, "seconds")
+
+execution_time_formatted = format_time(execution_time)
+
+print("Script execution time:", execution_time_formatted, "sedonds")
+print("Script execution time:", format_time(execution_time / 60), "minutes (decimal)")
+print("Script execution time:", format_time(execution_time/60/60), "hours (decimal)")
 
 #####################################################################################
