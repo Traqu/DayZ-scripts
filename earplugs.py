56120fca92389cbcf88d8917a2c85a6997d8c708
TraquPjatk <s19787@pjwstk.edu.pl>
2023-05-26 16:29:11 +0200

    Handled hotkeys detection while SHIFT is pressed simultaneously

--- earplugs.py
+++ earplugs.py
@@ -11,6 +11,8 @@ dayzSession = None
 escape_pressed = False
 allow_exit = True
 volume_ctrl = None
+shift_pressed = False
+n_pressed = False
 
 print("\n\nTo exit the application press ESC")
 
@@ -25,7 +27,27 @@ def on_escape_press(event):
         print("To exit - press END.")
 
 
+def on_shift_press(event):
+    global shift_pressed
+    shift_pressed = True
+
+def on_shift_release(event):
+    global shift_pressed
+    shift_pressed = False
+    
+def on_n_press(event):
+    global n_pressed
+    n_pressed = True
+
+def on_n_release(event):
+    global n_pressed
+    n_pressed = False
+    
 keyboard.on_press_key("esc", on_escape_press)
+keyboard.on_press_key("shift", on_shift_press)
+keyboard.on_release_key("shift", on_shift_release)
+keyboard.on_press_key("n", on_n_press)
+keyboard.on_release_key("n", on_n_release)
 
 while dayzSession is None and not escape_pressed:
     try:
@@ -58,12 +80,12 @@ if escape_pressed:
 else:
     def enableEarplugs(enableButton):
         if not earplugsEnabled.is_set():
-            if enableButton.name == 'n':
+            if enableButton.name == 'n' or n_pressed and shift_pressed:
                 earplugsEnabled.set()
                 print("Earplugs enabled")
                 volume_ctrl.SetMasterVolume(volume, None)
         else:
-            if enableButton.name == 'n':
+            if enableButton.name == 'n' or n_pressed and shift_pressed:
                 earplugsEnabled.clear()
                 print("Earplugs disabled")
                 volume_ctrl.SetMasterVolume(1, None)
@@ -71,13 +93,13 @@ else:
     def setVolume(volumeButton, volume_ctrl):
         global volume
         if earplugsEnabled.is_set():
-            if volumeButton.name == '=':
+            if volumeButton.name == '=' or volumeButton.name == '+':
                 if volume < 1:
                     volume += 0.1
                     volume = round(volume, 1)
                     volume_ctrl.SetMasterVolume(volume, None)
                     print("Volume set to: " + str(volume))
-            elif volumeButton.name == '-':
+            elif volumeButton.name == '-' or volumeButton.name == '_':
                 if volume > 0:
                     volume -= 0.1
                     volume = round(volume, 1)
@@ -91,4 +113,6 @@ else:
         if keyboard.is_pressed('end') or escape_pressed:
             break
 
-    print("Exiting application")
+if(volume_ctrl is not None):
+    volume_ctrl.SetMasterVolume(1, None)
+print("Exiting application")
