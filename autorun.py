33d2443222f388edfccb69199af2cb72bb32c79d
TraquPjatk <s19787@pjwstk.edu.pl>
2023-05-28 21:56:49 +0200

    Did some pupu to chat(enter). For unknown reason I am unable to do what I did with map, even though Im entering autorun() method. I just left it disabling AR 100% times

--- autorun.py
+++ autorun.py
@@ -11,11 +11,31 @@ autorun_hotkey_detected = False
 inventory_opened = False
 map_enabled = False
 should_you_let_me_in = False
+enter_pressed = False
+
+def handle_chat(chat_button):
+    global enter_pressed, autorun_enabled
+    autorun_enabled = False
+    keyboard.release('w')
+    keyboard.release('shift')
+    
+   # if(autorun_enabled):
+   #     autorun_enabled = False
+    
+#    if(autorun_enabled):
+#        keyboard.release('w')
+#        keyboard.release('shift')
+#        enter_pressed = True
+#        autorun_enabled = False
+#    elif(enter_pressed):
+#        print("ZAKURWIAMY DALEJ")
+#        time.sleep(0.1)
+#        autorun()
+#        enter_pressed = False
 
 
 def handle_map_button(map_button):
     global map_enabled, should_you_let_me_in
-    print("Autorun enabled: " + str(autorun_enabled))
     if(autorun_enabled or should_you_let_me_in):
         if(not map_enabled):
            force_stop_autorun()
@@ -23,28 +43,28 @@ def handle_map_button(map_button):
            should_you_let_me_in = True
         else:
             time.sleep(0.05)
-            start_autorun()
+            start_autorun(event=None)
             map_enabled = False 
             should_you_let_me_in = False
 
-def release_tab(dummy_arg=None):
+def release_tab(event):
     if(autorun_enabled):
         keyboard.release('tab')
 
 def autorun():
     keyboard.press('w')
     keyboard.press('shift')
+    print("Autorun enabled: " + str(autorun_enabled))
 
-def start_autorun(dummy_arg=None):
-    global autorun_hotkey_detected
-    global autorun_enabled
+def start_autorun(event):
+    global autorun_hotkey_detected, autorun_enabled
     if(not autorun_hotkey_detected):
         autorun_enabled = True
         autorun()
     else:
         autorun_hotkey_detected = False
 
-def start_autorun__giveaway_control(dummy_arg=None):
+def start_autorun__giveaway_control():
     global autorun_hotkey_detected
     global autorun_enabled
     autorun_hotkey_detected = True
@@ -52,7 +72,7 @@ def start_autorun__giveaway_control(dummy_arg=None):
     autorun_enabled = True
     autorun()
 
-def overtake_control(dummy_arg=None):
+def overtake_control(event):
     global autorun_enabled
     if autorun_enabled:
         autorun_enabled = False
@@ -97,7 +117,7 @@ def overlay_buttons_stop_autorun(overlay_button):
         keyboard.release('w')
         autorun_enabled = False
         time.sleep(0.1)
-        start_autorun()
+        start_autorun(event=None)
     
 
 def on_w_release(e):
@@ -121,7 +141,8 @@ keyboard.add_hotkey(HOTKEYS_SHIFT_W_AR, start_autorun__giveaway_control)
 mouse.on_right_click(force_stop_autorun)
 keyboard.on_press(release_tab)
 keyboard.on_press_key('m', handle_map_button)
-
+keyboard.on_press_key('enter', handle_chat) 
+# to uwidacznia problemy NIEwyłączania sie runa w niektórych sytuacjach (debug)
 
 while True:
     if autorun_enabled:
